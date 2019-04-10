3d0cfdd946b21652971e676f4f37c3cb2dc950f6
Silvio Schwarz <admin@silvioschwarz.com>
2019-04-10 06:28:10 +0100

    made ready for publication, kinda

--- deployRaspi.sh
+++ deployRaspi.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 #git clone https://github.com/silvioschwarz/silvioschwarz.com.git
 
-echo "pull github repo"
-git pull
+#echo "pull github repo"
+#git pull
 
 echo "install gems"
 bundle install
@@ -11,6 +11,6 @@ echo "deploy"
 bundle exec jekyll build
 
 echo "copy to /var/www/html"
-sudo cp -r _site/* /var/www/html/
+sudo scp -r _site/* pi@silvioschwarz.com:/var/www/html/
 
 echo "DONE!"
