bb936f9186de5a067a12f0b4e51e7a3fb50193fa
Silvio Schwarz <admin@silvioschwarz.com>
2019-04-26 04:02:32 +0200

    updated

--- _site/deploySite.sh
+++ _site/deploySite.sh
@@ -3,17 +3,8 @@
 git pull
 bundle install
 bundle exec jekyll build
-<<<<<<< HEAD
 
 git push
-=======
-<<<<<<< HEAD
-#sudo cp -r website/* /var/www/html/
-
-rsync -azP --delete _site/* /var/www/html/
-=======
->>>>>>> b5985b4f393d7a09728e1192aeb0e5f0c33814e0
 #sudo cp -r _site/* /var/www/html/
 
 sudo rsync -azP --delete _site/* /var/www/html/
->>>>>>> 78d0d4ff6f19440a699c25be7991d0afae9d2251
